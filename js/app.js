60ff4fbe4d4ab1d928a3a4051fdbedb6e2298740
Nina-K-Welch <ninakwelch@gmail.com>
2018-06-20 16:11:33 +0100

    Collision detection added

--- js/app.js
+++ js/app.js
@@ -1,3 +1,7 @@
+// variables
+var lives = 5;
+
+
 // Enemy constructor function
 var Enemy = function(x, y) {
     // x and y coordinates for the enemy objects on the canvas
@@ -37,13 +41,13 @@ var Player = function(x, y) {
       ctx.drawImage(Resources.get(this.sprite), this.x, this.y);
     };
     this.handleInput = function(keyCode) {
-        if(keyCode === 'left' && player.x > 0) {
-            player.x -=100;
+        if(keyCode === 'left' && player.x > 100) {
+            player.x -=101;
         } else if(keyCode === 'up' && player.y > 0) {
             player.y -=83;
-        } else if(keyCode === 'right' && player.x < 400) {
-            player.x +=100
-        } else if(keyCode === 'down' && player.y < 400) {
+        } else if(keyCode === 'right' && player.x < 404) {
+            player.x +=101;
+        } else if(keyCode === 'down' && player.y < 392) {
             player.y += 83;
         }
     }
@@ -60,8 +64,51 @@ var enemy3 = new Enemy(-100, 226);
 var allEnemies = [enemy1, enemy2, enemy3];
 
 // Place the player object in a variable called player
-var player = new Player(200, 400);
+var player = new Player(202, 392);
 
+// Check if the player and an enemy collide before player reaches the top
+function checkCollisions() {
+    // loop enemies array
+    for(var i = 0; i < allEnemies.length; i++) {
+        var enemy = allEnemies[i];
+
+        // check if the player and an enemy collide
+        if(enemy.y === player.y && enemy.x > (player.x - 50) && enemy.x < (player.x + 50)) {
+
+            // reduce lives
+            lives--;
+
+            // end game if there are no more lives left
+            if(!lives) {
+                alert('Game over!');
+            } else {
+                // reset the game
+                startOver();
+            }
+        } else if(player.y < 0) {
+            // end game if the player reaches the top without colliding
+            setTimeout(function() {
+                alert('You won!');
+            }, 200);
+        }
+    }
+}
+
+// reset the game board
+function startOver() {
+
+    //reset position for player
+    player.x = 202;
+    player.y = 392;
+
+    //reset position for enemies
+    enemy1.x = -100;
+    enemy1.y = 60;
+    enemy2.x = -100;
+    enemy2.y = 143;
+    enemy3.x = -100;
+    enemy3.y = 226;
+}
 
 // This listens for key presses and sends the keys to your
 // Player.handleInput() method. You don't need to modify this.
